af46034ab0f1be98128d4ce34470f99da5f33979
athphane <athphane@gmail.com>
2024-04-23 14:34:23 +0500

    - ability to pass in options to a ajax select. a just in case feature

--- resources/views/field-components/api-select.blade.php
+++ resources/views/field-components/api-select.blade.php
@@ -1,5 +1,7 @@
 <x-forms::select2
     :name="$getName()"
+    :options="$getOptions()"
+    :default="$getSelected()"
     :ajax-url="route('api.islands.index')"
     :filter-field="$getFilterBy()"
     :required="$isMarkedAsRequired()"
