5cd92a57a768d30bc0ea3371cf629ebf30fd15d2
fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2024-04-25 08:19:07 +0500

    feat: use forms package for text input fields

--- resources/views/field-components/text-input.blade.php
+++ resources/views/field-components/text-input.blade.php
@@ -1,25 +1,18 @@
-<x-paperless::input-group
-    for="{{ $getId() }}"
-    :label="$getLabel()"
-    :required="$isMarkedAsRequired()"
->
-    @php
-        $name = $getName();
-        $id = $getId();
-        if ($isRepeatable()) {
-            $repeating_instance = $getRepeatingInstance();
-            $id .= "_{$repeating_instance}";
-            $name = "{$getRepeatingGroupId()}[{$repeating_instance}][$name]";
-        }
+@php
+    $name = $getName();
+    $id = $getId();
+    if ($isRepeatable()) {
+        $repeating_instance = $getRepeatingInstance();
+        $id .= "_{$repeating_instance}";
+        $name = "{$getRepeatingGroupId()}[{$repeating_instance}][$name]";
+    }
 
-    @endphp
-    <x-paperless::text-input
-        :type="$getType()"
-        :dv="$hasDhivehiInput()"
-        id="{{ $id }}"
-        name="{{ $name }}"
-        :required="$isMarkedAsRequired()"
-        :value="$getState()"
-        :placeholder="$getPlaceholder()"
-    />
-</x-paperless::input-group>
+@endphp
+
+<x-forms::text
+    name="{{ $name }}"
+    :value="$getState()"
+    :placeholder="$getPlaceholder()"
+    :required="$isMarkedAsRequired()"
+    inline
+/>
