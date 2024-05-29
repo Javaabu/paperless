cbda00e535bcd32f8d9bc02da90c7679124b3dc9
Athfan Khaleel <athphane@gmail.com>
2024-05-29 13:42:11 +0500

    - add margins directly to the conditional wrapper

--- resources/views/components/conditional-wrapper.blade.php
+++ resources/views/components/conditional-wrapper.blade.php
@@ -3,10 +3,11 @@
     'enableElem',
     'value',
     'hideFields' => false,
+    'class' => 'mb-5'
 ])
 
 @if($isConditional)
-<x-forms::conditional-wrapper :enable-elem="$enableElem" :enable-value="$value" :hide-fields="true" :disable="$hideFields">
+<x-forms::conditional-wrapper :class="$class" :enable-elem="$enableElem" :enable-value="$value" :hide-fields="true" :disable="$hideFields">
     {{ $slot }}
 </x-forms::conditional-wrapper>
 @else
